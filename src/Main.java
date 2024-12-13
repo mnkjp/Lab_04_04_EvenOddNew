1b62c84eb61a09025f8a15e82d0113a0ea87f259
mnkjp <menkejp@mail.uc.edu>
2024-12-13 17:05:36 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+
+
+        System.out.println("numToExamine: " + numToExamine);
+        if(numToExamine % 2 == 0)
+            System.out.println("The number to be examined is even");
+        else
+            System.out.println("The number to be examined is odd");
+    }
+}
